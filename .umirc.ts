77922d88ef5da00b2127361c2dd176b1b80d0719
张显磊 <1079105171@qq.com>
2020-03-13 20:37:13 +0800

    feat(index.js): 富文本编辑器
    添加了富文本编辑器,可以使用

--- .umirc.ts
+++ .umirc.ts
@@ -3,10 +3,12 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   routes: [
     {
-      path: '/', component: '@/pages/index',
-      routes: [
-        { path: '/login', component: './login/index' }
-      ]
+      path: '/',
+      component: '@/pages/index',
+      routes: [{ path: '/login', component: './login/index' }],
     },
   ],
+  cssLoaderOptions: {
+    localIdentName: '[local]',
+  },
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './index.less';
+import 'react-quill/dist/quill.snow.css';
+import './fixed.css';
 
 import { Layout, Menu, Form, Input, Button, Checkbox } from 'antd';
 import {
@@ -7,11 +9,11 @@ import {
   UploadOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
+import ReactQuill from 'react-quill';
 
 const { Content, Footer, Sider } = Layout;
 
 class BaseForm extends React.Component {
-
   render() {
     const layout = {
       labelCol: { span: 8 },
@@ -27,120 +29,178 @@ class BaseForm extends React.Component {
 
     const onFinishFailed = (errorInfo: any) => {
       console.log('Failed:', errorInfo);
-    }
+    };
 
-    return <Form
-      {...layout}
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      layout='horizontal'
-    // colon={false}
-    // validateMessages={{ required: "用户名不能为空" }}
-    >
-      <Form.Item
-        label="用户名"
-        name="username"
-        rules={[
-          { required: true, message: '请输入用户名' },
-          { min: 3, message: '长度须大于3' },
-          { max: 10, message: '长度须小于10' },
-          // { whitespace: true, message:'请输入用户名'}
-        ]}
-        // labelCol={{span:5,offset:0}}
-        // extra="用户名不能为空"
-        // help="这是帮助信息"
-        normalize={(value, prevValue, prevValues) => { console.log(value); return value.toString() }}
+    return (
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+          fontShift: '每次按下都会在内容最后加1',
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        layout="horizontal"
+        // colon={false}
+        // validateMessages={{ required: "用户名不能为空" }}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[
+            { required: true, message: '请输入用户名' },
+            { min: 3, message: '长度须大于3' },
+            { max: 10, message: '长度须小于10' },
+            // { whitespace: true, message:'请输入用户名'}
+          ]}
+          // labelCol={{span:5,offset:0}}
+          // extra="用户名不能为空"
+          // help="这是帮助信息"
+          normalize={(value, prevValue, prevValues) => {
+            console.log(value);
+            return value.toString();
+          }}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        // labelAlign="left"
-        label="密码"
-        name="password"
-        rules={[
-          { required: true, message: '请输入密码' },
-          { min: 3, message: '长度须大于6' },
-          { max: 10, message: '长度须小于16' },
-        ]}
+        <Form.Item
+          // labelAlign="left"
+          label="密码"
+          name="password"
+          rules={[
+            { required: true, message: '请输入密码' },
+            { min: 3, message: '长度须大于6' },
+            { max: 10, message: '长度须小于16' },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          // labelAlign="left"
+          label="邮箱"
+          name="email"
+          rules={[
+            {
+              pattern: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
+              message: '邮箱不匹配',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        // labelAlign="left"
-        label="邮箱"
-        name="email"
-        rules={[{ pattern: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/, message: '邮箱不匹配' }]}
+        <Form.Item
+          // labelAlign="left"
+          label="字段转换"
+          name="fontShift"
+          rules={[]}
+          normalize={value => value.toString() + 1}
+        >
+          <Input />
+        </Form.Item>
 
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>记住账户</Checkbox>
+        </Form.Item>
 
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>记住账户</Checkbox>
-      </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          提交
-      </Button>
-      </Form.Item>
-    </Form >
+class ListTrace extends React.Component {
+  state = {
+    listData: () => {
+      let i = 0;
+      let arr = [];
+      while (i < 10) {
+        arr.push({ title: `Ant Design Title ${i}` });
+        i += 1;
+      }
+      return arr;
+    },
+  };
+  render() {
+    const { listData } = this.state;
+    return <></>;
   }
 }
 
 class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+  }
+  state = {
+    text: '',
+  };
+  handleChange() {}
   render() {
-    return <>
-      <div className={styles.header}>
-        后台管理项目
-      </div>
+    return (
+      <>
+        <div className={styles.header}>后台管理项目</div>
 
-      <div className={styles.body}>
-        <Layout style={{ height: "100%" }}>
-          <Sider
-            style={{
-              overflow: 'auto',
-              height: '100vh',
-              position: 'fixed',
-              left: 0,
-            }}
-          >
-            <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-              <Menu.Item key="1">
-                <UserOutlined />
-                <span className="nav-text">nav 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <VideoCameraOutlined />
-                <span className="nav-text">nav 2</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <UploadOutlined />
-                <span className="nav-text">nav 3</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
+        <div className={styles.body}>
+          <Layout style={{ height: '100%' }}>
+            <Sider
+              style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0,
+              }}
+            >
+              <div className="logo" />
+              <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                <Menu.Item key="1">
+                  <UserOutlined />
+                  <span className="nav-text">nav 1</span>
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <VideoCameraOutlined />
+                  <span className="nav-text">nav 2</span>
+                </Menu.Item>
+                <Menu.Item key="3">
+                  <UploadOutlined />
+                  <span className="nav-text">nav 3</span>
+                </Menu.Item>
+              </Menu>
+            </Sider>
+            <Layout className="site-layout" style={{ marginLeft: 200 }}>
+              <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                {/* 表单部分 */}
+                <div
+                  className="site-layout-background"
+                  style={{ padding: 24, textAlign: 'center' }}
+                >
+                  {/* 表单 */}
+                  <BaseForm></BaseForm>
+                </div>
 
-          <Layout className="site-layout" style={{ marginLeft: 200 }} >
+                {/* 列表部分 */}
+                <ListTrace></ListTrace>
 
-            <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-              <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
-                {/* 表单 */}
-                <BaseForm></BaseForm>
-              </div>
-            </Content>
-
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                {/* 富文本编辑器 */}
+                <ReactQuill
+                  value={this.state.text}
+                  onChange={this.handleChange}
+                />
+              </Content>
+              <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©2018 Created by Ant UED
+              </Footer>
+            </Layout>
           </Layout>
-        </Layout>
-      </div>
-    </>
+        </div>
+      </>
+    );
   }
 }
 
